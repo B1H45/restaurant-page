46fc44d34cdca3f8c167da866bcbaec9192bb11c
Ben Hustler <ben1hustler@gmail.com>
2026-03-02 05:01:36 +0000

    Finishing page navigation, no visuals implemented

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 import "./styles.css";
-import { loadPage } from "./load-page.js";
+import { loadHome } from "./home.js";
+import { loadAbout } from "./about.js";
+import { loadMenu } from "./menu.js";
 
 console.log("THING");
 
-loadPage();
+let navButtons = document.querySelectorAll("nav>button");
+const content = document.getElementById("content");
+
+navButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        content.replaceChildren();
+        switch (e.target.id) {
+            case "home":
+                loadHome();
+                break;
+            case "about":
+                loadAbout();
+                break;
+            case "menu":
+                loadMenu();
+                break;
+        }
+    });
+})
+
+loadHome();
